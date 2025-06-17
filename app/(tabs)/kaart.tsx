6a9060f1496a86aec89e6fd54e6306c3a0f8a231
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-06-18 00:07:52 +0200

    google maps kaart toegevoegd

--- app/(tabs)/kaart.tsx
+++ app/(tabs)/kaart.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import MapStyle from "../../constants/mapstyles.json";
 
-const kaart = () => {
+const Kaart = () => {
   return (
-    <View>
-      <Text>kaart</Text>
-    </View>
-  )
-}
+    <MapView
+      style={StyleSheet.absoluteFillObject}
+      initialRegion={{
+        latitude: 52.3702,
+        longitude: 4.8952,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }}
+      provider="google"
+      customMapStyle={MapStyle}
+    />
+  );
+};
 
-export default kaart
+export default Kaart;
